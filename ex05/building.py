afb7a82bc97e22fca8f28f6124a10365e0cb73c1
Nicolas Stoutzer <nstoutze@42nice.fr>
2024-11-11 16:11:04 +0100

    ex05 DONE

--- ex05/building.py
+++ ex05/building.py
@@ -2,34 +2,28 @@
 
 import sys
 
-"""
-#!/usr/bin/env python3
 
-import sys
+def main() -> None:
+    """
+    Counts different sort of character (upper-case characters,
+    lower-case characters, punctuation characters,
+    digits and spaces)in the text argument received from the shell,
+    and prints the result.
+    
+    If no argument is received from the shell, it is possible to
+    type it on several lines.
+    Each \\n typed through the Entry key is turned into a space.
+    Send an EOF signal (through Ctrl+D) to end your text.
+    """
+    
+    DEBUG = 0
 
-def whatis() -> None:
-	nb_args = len(sys.argv) - 1
-	if nb_args == 0 : exit()
-	assert nb_args == 1, ("more than one argument is provided")
-
-	try : arg = int(sys.argv[1])
-	except : raise AssertionError("argument must be an integer")
-
-	parity = "Odd" if arg%2 else "Even"
-	print(f"I'm {parity}.")
-
-if __name__ == "__main__" :
-	try : whatis()
-	except AssertionError as error : 
-		print(f"{type(error).__name__}: {error}", file=sys.stderr)
-"""
-
-def main():
-    """docstring"""
-
-    nb_args = len(sys.argv) - 1
-    text = ""
-    if nb_args == 0:
+    def get_input() -> str:
+        """Gets the input in case of no argument is provided in the CLI.
+        Each carriage return is turned into a space.
+        End of capture occurs with an EOF signal (lauched with Ctrl+D).
+        Returns a string which contains each line, space (not \\n) separated.
+        """
         print("What is the text to count?")
         text = list()
         while True:
@@ -42,7 +36,50 @@ def main():
         if len(text) == 0:
             print("No input provided.")
             exit(1)
-    print(f"text = {text}")
+        return text
+    
+
+    def building_count_function(text: str) -> None:
+        """
+        Displays several numbers, according to the text(a string)
+        argument received ; number of :
+        - upper letter(s)
+        - lower letter(s)
+        - punctuation mark(s)
+        - space(s)
+        - digit(s)
+        """
+        punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+        criteria = {"upper letters": lambda c: c.isupper(),
+                    "lower letters": lambda c: c.islower(),
+                    "punctuation marks": lambda c: c in punctuation,
+                    "spaces" : lambda c: c.isspace(),
+                    "digits" : lambda c: c.isdigit()}
+        
+        counts = {
+            name: sum(1 for c in text if check(c))
+            for name, check in criteria.items()}
+
+        print(f"The text contains {len(text)} characters:")
+        for name, count in counts.items():
+            print(f"{count} {name}")
+
+    nb_args = len(sys.argv) - 1
+    try :
+        assert nb_args < 2, ("more than one argument is provided")
+    except AssertionError as error :
+        print(f"{type(error).__name__}: {error}", file=sys.stderr)
+        exit(1)
+
+    text = ""
+    if nb_args == 0:
+        text = get_input()
+    elif nb_args == 1:
+        text = sys.argv[1]
+    if DEBUG:
+        print(f"text = {text}")
+
+    building_count_function(text)
 
 
 if __name__ == "__main__":
