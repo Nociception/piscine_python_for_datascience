946549b5e40c7de2f24e76a7bda57e1b7fee9bec
Nicolas Stoutzer <nstoutze@42nice.fr>
2025-01-07 18:12:16 +0100

    Day03/ex04/tester.py added

--- Python-3-OOP/ex04/tester.py
+++ Python-3-OOP/ex04/tester.py
@@ -1,3 +1,23 @@
+# Subject
+"""
+Your tester.py:
+from ft_calculator import calculator
+a = [5, 10, 2]
+b = [2, 4, 3]
+calculator.dotproduct(a,b)
+calculator.add_vec(a,b)
+calculator.sous_vec(a,b)
+
+Expected output: (docstrings can be different)
+$> python tester.py
+Dot product is: 56
+Add Vector is : [7.0, 14.0, 5.0]
+Sous Vector is: [3.0, 6.0, -1.0]
+$>
+You don't have to do any error handling
+
+"""
+
 from ft_calculator import calculator
 
 a = [5, 10, 2]
